7e18d7feb42f1cdd7c719de18b4c0b6540ed376e
ikhyeon kim <101814294+kimikhyeon1@users.noreply.github.com>
2022-12-09 10:59:48 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
@@ -1,39 +1,23 @@
 def solution(lines):
     total_space = [0] * 202
 
-    for i in range(lines[0][0], lines[0][1] + 1):
+    for i in range(lines[0][0]+1, lines[0][1] + 1):
         total_space[i + 100] += 1
 
-    for i in range(lines[1][0], lines[1][1] + 1):
+    for i in range(lines[1][0]+1, lines[1][1] + 1):
         total_space[i + 100] += 1
 
-    for i in range(lines[2][0], lines[2][1] + 1):
+    for i in range(lines[2][0]+1, lines[2][1] + 1):
         total_space[i + 100] += 1
-
-    if lines[0][1] == lines [1][0]:
-        total_space[lines[0][1]+100] -=1
-
-    if lines[1][1] == lines [2][0]:
-        total_space[lines[0][1]+100] -=1
-
-    if lines[0][1] == lines [2][0]:
-        total_space[lines[0][1]+100] -=1
-
-    if lines[0][0] == lines [1][1]:
-        total_space[lines[0][1]+100] -=1
-
-    if lines[1][0] == lines [2][1]:
-        total_space[lines[0][1]+100] -=1
-
-    if lines[2][1] == lines [0][0]:
-        total_space[lines[0][1]+100] -=1
-
+    
     answer =0
-
-
+    
+    print(total_space)
     for i in range(1,len(total_space)):
-        if total_space[i] > 1 and total_space[i-1] > 1:
+        if total_space[i] > 1 :
             answer +=1
 
-    print(total_space)
     return answer
+
+
+print(solution([[1, 3], [1, 6], [1, 10]]))
