f052db3476f73af343c848ddfbf858d447ca6065
ikhyeon kim <101814294+kimikhyeon1@users.noreply.github.com>
2023-01-14 02:32:43 +0900

    [level 1] Title: 소수 찾기, Time: 25.51 ms, Memory: 55.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
@@ -2,13 +2,12 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         int[] array = new int[n+1];
-        array[0] = array[1] =0;
         for (int i = 2; i <=n ; i++) {
             array[i]=i;
         }
-        for (int i = 2; i <n ; i++) {
+        for (int i = 2; i*i <=n ; i++) {
             if (array[i]==0)continue;
-            for (int j = i*2; j <=n ; j+=i) {
+            for (int j = i*i; j <=n ; j+=i) {
                 array[j]=0;
             }
         }
