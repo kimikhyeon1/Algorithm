a2003d2d91984a318658233b8c4c5d1c544df4cd
ikhyeon kim <101814294+kimikhyeon1@users.noreply.github.com>
2023-03-23 13:21:54 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 86.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] common) {
+        int answer = 0;
+        if (common[1] - common[0] == common[2] - common[1]) {
+            int a = common[1] - common[0];
+            answer = common[common.length - 1] + a;
+        } else {
+            int a = common[common.length - 1] / common[common.length - 2];
+            answer = common[common.length - 1] * a;
+        }
+        return answer;
+    }
+}
