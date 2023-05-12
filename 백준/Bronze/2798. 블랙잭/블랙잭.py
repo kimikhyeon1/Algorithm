d3ac2701d4da33974a244f69f1c85eed4325ac98
ikhyeon kim <101814294+kimikhyeon1@users.noreply.github.com>
2023-05-13 02:59:35 +0900

    [Bronze II] Title: 블랙잭, Time: 96 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -0,0 +1,23 @@
+N, target = map(int,input().split())
+array = list(map(int,input().split()))
+
+count = 0
+answer = 0
+def bfs(index,csum):
+    global count
+    global answer
+    if csum > target:
+        return
+    if count ==3:
+        answer = max(answer,csum)
+        return
+
+    for i in range(index,N):
+        count+=1
+        bfs(i+1,csum+array[i])
+        count-=1
+
+
+bfs(0,0)
+
+print(answer)
