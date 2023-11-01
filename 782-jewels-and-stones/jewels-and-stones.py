924803a49507bdcd9cc2e350bddba6e568a2084f
ikhyeon kim <101814294+kimikhyeon1@users.noreply.github.com>
2023-11-01 09:52:17 +0900

    Time: 39 ms (52.82%) | Memory: 16.1 MB (70.66%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.py
+++ 782-jewels-and-stones/jewels-and-stones.py
@@ -1,10 +1,17 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
         answer = 0
-        for ch in jewels:
-            for a in stones:
-                if ch == a:
-                    answer += 1
+        dic = {}
+
+        for i in stones:
+            if i not in dic:
+                dic[i] = 1
+            else:
+                dic[i] += 1
+        
+        for i in jewels:
+            if i in dic:
+                answer += dic[i]
 
         return answer
         
