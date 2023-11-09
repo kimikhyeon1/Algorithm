52c8684b7a4137be0833e5ffa278debea67d1fbc
ikhyeon kim <101814294+kimikhyeon1@users.noreply.github.com>
2023-11-09 13:21:56 +0900

    Time: 55 ms (6.06%) | Memory: 16.4 MB (43.38%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,4 +1,19 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        return list(itertools.permutations(nums))
-        
+
+        def DFS(array):
+            if len(array) == len(nums):
+                answer.append(array[:])
+                return
+            
+            for i in range(len(nums)):
+                if nums[i] in array:
+                    continue
+                array.append(nums[i])
+                DFS(array)
+                array.pop()
+
+        answer = []
+        array = []
+        DFS(array)
+        return answer
