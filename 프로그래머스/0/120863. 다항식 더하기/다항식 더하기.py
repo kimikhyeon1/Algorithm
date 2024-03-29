fa1f86bf3ed8773a9ff3e7bfddddec2aff0d8217
Ikhyeon KIM <101814294+kimikhyeon1@users.noreply.github.com>
2024-03-29 13:45:47 +0900

    [level 0] Title: 다항식 더하기, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.py
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.py
@@ -1,49 +1,31 @@
 def solution(polynomial):
-    number = 0
+    answer = ''
     x = 0
-    stack = list(polynomial)
-    number_space = ""
-    is_x = False
-    while stack:
-        temp = stack.pop()
-        
-        if temp == " ":
-            if is_x:
-                if number_space:
-                    x += int(number_space[::-1])
-                else:
-                    x += 1
-                is_x = False
-            else:
-                if number_space:
-                    number += int(number_space[::-1])
-            number_space = ""
-            
-        if temp == "x":
-            is_x = True
-        
-        if 45 < ord(temp) < 65:
-            number_space += temp
+    number = 0
     
-    if is_x:
-        if number_space:
-            x += int(number_space[::-1])
-        else:
-            x += 1
-    elif number_space:
-        number += int(number_space[::-1])
-            
-    answer = ""
+    for i in range(len(polynomial)):
+        if polynomial[i] == 'x':
+            if i == 0 or polynomial[i - 1] == " ":
+                x += 1
+            else:
+                x += int(polynomial[i - 1])
+            continue
+        
+        if 48 < ord(polynomial[i]) < 65:
+            if i == len(polynomial) - 1:
+                number += int(polynomial[i])
+            elif polynomial[i + 1] != 'x':
+                number += int(polynomial[i])
     
     if x == 1:
-        answer += "x"
-    elif x > 1:
-        answer += str(x)+"x"
+        answer = 'x'
+    if x > 1:
+        answer = str(x) + 'x'
     
     if number > 0:
-        if x:
+        if len(answer) > 0:
             answer += " + " + str(number)
         else:
-            answer += str(number)
-            
+            answer = str(number)
+                
     return answer
