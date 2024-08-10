3ca90a4fde294e91bc920889090615eb5959e85b
Ikhyeon KIM <101814294+kimikhyeon1@users.noreply.github.com>
2024-08-10 19:48:56 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 2702.09 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
@@ -0,0 +1,10 @@
+def solution(n):
+    for i in range(1,n+1):
+        if i ** 2 == n :
+            return ( i + 1 ) ** 2
+        
+        if i ** 2 > n:
+            break
+        
+    return -1 
+        
